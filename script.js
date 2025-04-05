79cfc033642b5348b84825b1c857bc2e1317a1d8
Satyam-nitp <satyamsingh2004alld@gmail.com>
2025-04-05 20:54:43 +0530

    files modified using single variable i

--- script.js
+++ script.js
@@ -3,17 +3,13 @@ const dabba=document.querySelector(".images");
 const clc=document.querySelectorAll(".i");
 
 let i=0;
-let j=0;
 
 const leftMove=()=>{
     if(i===0) i=5;
-    if(j===-1) j=4;
     i--;
-    j--;
     let pos=(-350*i);
     dabba.style.left = `${pos}px`;
     console.log(i)
-    console.log(j)
     clc.forEach((el)=>{
         if(el.classList.contains(`i${i}`)){
             el.classList.add("active")
@@ -23,16 +19,13 @@ const leftMove=()=>{
 }
 
 const rightMove=()=>{
-    if(j===4) j=-1;
     if(i===4) i=-1;
-    j++;
     i++;
-    let pos=(-350*j);
+    let pos=(-350*i);
     dabba.style.left = `${pos}px`;
     console.log(i)
-    console.log(j)
     clc.forEach((el)=>{
-        if(el.classList.contains(`i${j}`)){
+        if(el.classList.contains(`i${i}`)){
             el.classList.add("active")
         }
         else el.classList.remove("active");

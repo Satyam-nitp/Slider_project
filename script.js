d7df018e3c3f69fec07ec8931bd129c52fc11197
Satyam-nitp <satyamsingh2004alld@gmail.com>
2025-04-05 20:52:04 +0530

    files modified

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
 
 const dabba=document.querySelector(".images");
+const clc=document.querySelectorAll(".i");
 
 let i=0;
 let j=0;
 
 const leftMove=()=>{
-    let poss=(-350*i);
-    console.log("Hello world");
-    console.log(poss);
-    dabba.style.left = `${poss}px`;
-    i++;
-    if(i===5) i=0;
+    if(i===0) i=5;
+    if(j===-1) j=4;
+    i--;
+    j--;
+    let pos=(-350*i);
+    dabba.style.left = `${pos}px`;
+    console.log(i)
+    console.log(j)
+    clc.forEach((el)=>{
+        if(el.classList.contains(`i${i}`)){
+            el.classList.add("active")
+        }
+        else el.classList.remove("active");
+    })
 }
 
 const rightMove=()=>{
-    let pos=(-350*j-350);
-    console.log("Hello world");
-    console.log(pos);
-    dabba.style.left = `${pos}px`;
-    j++;
     if(j===4) j=-1;
+    if(i===4) i=-1;
+    j++;
+    i++;
+    let pos=(-350*j);
+    dabba.style.left = `${pos}px`;
+    console.log(i)
+    console.log(j)
+    clc.forEach((el)=>{
+        if(el.classList.contains(`i${j}`)){
+            el.classList.add("active")
+        }
+        else el.classList.remove("active");
+    })
 }
-
